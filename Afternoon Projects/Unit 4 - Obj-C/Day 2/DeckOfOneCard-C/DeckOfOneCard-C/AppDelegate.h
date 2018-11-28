9116ee67a110a24cbeeba607873e83661e30c0fd
fmartin0212 <fmartin0212@gmail.com>
2018-11-28 10:48:09 -0700

    initial commit, includes all starter projects

---
+++ Afternoon Projects/Unit 4 - Obj-C/Day 2/DeckOfOneCard-C/DeckOfOneCard-C/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  DeckOfOneCard-C
+//
+//  Copyright © 2018 DevMtnStudent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Afternoon Projects/Unit 4 - Obj-C/Day 2/DeckOfOneCard-C/DeckOfOneCard-C/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  DeckOfOneCard-C
+//
+//  Copyright © 2018 DevMtnStudent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
